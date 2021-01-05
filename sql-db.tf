7752fbbef6b08ce7b134c0b6323e848de0f9a60f
Naveen080 <37764648+Naveen080@users.noreply.github.com>
2021-01-05 22:17:19 +0530

    Update sql-db.tf

--- sql-db.tf
+++ sql-db.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "akssa1"
+  name                     = "aksrgtest1"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
